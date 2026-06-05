71aad94342e1471d92e1f7fd3618656e3873de48
Thomas W. <96929397+ctwyrth@users.noreply.github.com>
2026-06-05 10:43:30 -0700

    3rd - added --verbose and a message stack for ongoing conversation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY") or RuntimeError("GEMINI_API_KEY not found in environment variables")
@@ -11,24 +12,30 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="AlexanderAI")
 parser.add_argument("user_prompt", type=str, help="The prompt to send to the AI model")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 def main():
-    print("Hello from AlexanderAI!")
+  print("Hello from AlexanderAI!")
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=args.user_prompt
-    )
+  messages: list[types.Content] = [
+      types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+  ]
 
+  response = client.models.generate_content(
+      model="gemini-2.5-flash",
+      contents=messages
+  )
+
+  if args.verbose and response.usage_metadata is not None:
     print("User prompt: ", args.user_prompt)
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ", response.usage_metadata.candidates_token_count)
 
-    if response.usage_metadata is not None:
-      print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-      print("Response tokens: ", response.usage_metadata.candidates_token_count)
-      print("Response: ", response.text)
-    else:
-      raise RuntimeError("Response usage metadata is None, cannot print token counts.")
+  if response.usage_metadata is not None:
+    print("Response: ", response.text)
+  else:
+    raise RuntimeError("Response usage metadata is None, cannot print token counts.")
 
 if __name__ == "__main__":
     main()
